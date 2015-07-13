633b57ba4dd5050e0a7ad326b429953f57c1f0c4
YoBiya <yobiya@dummy.com>
2015-07-13 23:21:57 +0900

    ファイルの読み込み処理を追加

--- main.hs
+++ main.hs
@@ -1,6 +1,15 @@
+-- JSON Constitution Checker
+
 import System.Environment
+import Control.Exception
 
 main = do
   args <- getArgs
-  mapM putStrLn args
+  let fileName = (head args)
+  fileContents <- catch (readFile fileName) (readErrorHander fileName)
+  putStr fileContents;
 
+readErrorHander :: String -> IOError -> IO String
+readErrorHander fileName error = do
+  putStrLn $ "Can not open File \"" ++ fileName ++ "\""
+  return []
