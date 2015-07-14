a8ba239565cc6e0d4f2e23cdefb9d42cc5899656
YoBiya <yobiya@dummy.com>
2015-07-15 00:31:54 +0900

    構造ファイルとターゲットファイルの内容を表示するように対応

--- main.hs
+++ main.hs
@@ -5,11 +5,13 @@ import Control.Exception
 
 main = do
   args <- getArgs
-  let fileName = (head args)
-  fileContents <- catch (readFile fileName) (readErrorHander fileName)
-  putStr fileContents;
+  let c:constitutionFileName:t:targetFileName:_ = args
+  constitution <- catch (readFile constitutionFileName) (readErrorHander constitutionFileName)
+  target <- catch (readFile targetFileName) (readErrorHander targetFileName)
+  putStrLn constitution
+  putStrLn target
 
 readErrorHander :: String -> IOError -> IO String
 readErrorHander fileName error = do
   putStrLn $ "Can not open File \"" ++ fileName ++ "\""
-  return []
+  return ""
