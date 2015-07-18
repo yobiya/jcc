8706df2f6ac4e34d7dda5920637f958153222cc7
YoBiya <yobiya@dummy.com>
2015-07-18 20:26:31 +0900

    囲まれている要素を取り出す関数を追加

--- json.hs
+++ json.hs
@@ -20,6 +20,21 @@ parseObject :: String -> Either String JsonObject
 parseObject (x:xs)  | x == '{' && last xs == '}'  = Right $ parseObjectContents $ init xs
                     | otherwise                   = Left "Not found JSON object {} pair."
 
+-- 囲まれている要素を取り出す
+bracketContent :: String -> Char -> Char -> String
+bracketContent text sb eb = bracketContentLevel text sb eb 0
+
+bracketContentLevel :: String -> Char -> Char -> Int -> String
+bracketContentLevel "" _ _ _                        = ""
+bracketContentLevel (x:xs) sb eb 0      | x == sb   = (bracketContentLevel xs sb eb 1)
+                                        | otherwise = bracketContentLevel xs sb eb 0
+bracketContentLevel (x:xs) sb eb 1      | x == sb   = x:(bracketContentLevel xs sb eb 2)
+                                        | x == eb   = ""
+                                        | otherwise = x:(bracketContentLevel xs sb eb 1)
+bracketContentLevel (x:xs) sb eb level  | x == sb   = x:(bracketContentLevel xs sb eb (level + 1))
+                                        | x == eb   = x:(bracketContentLevel xs sb eb (level - 1))
+                                        | otherwise = x:(bracketContentLevel xs sb eb level)
+
 -- JSONのオブジェクトの要素をパースする
 parseObjectContents :: String -> JsonObject
 parseObjectContents text = [("a", JsonValue { valueType="dummy", value=head $ dissolveObjectContentStrings text, object=Nothing, array=[]})]
