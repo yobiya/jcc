7e363864afa810d2e94544f143368b09ef4d496b
YoBiya <yobiya@dummy.com>
2015-07-28 22:34:09 +0900

    必要な構成のキーがなかった場合に、適切なエラーメッセージを表示する処理を追加

--- match.hs
+++ match.hs
@@ -20,69 +20,69 @@ matchConstitution t c = let maybeEntryName = lookup "entry" c
                             False ->  "error"
                             True  ->  let (JsonObject object) = fromJust maybeObject
                                           (JsonString entoryName) = fromJust maybeEntryName
-                                      in  matchTypeFromName t object entoryName
+                                      in  matchTypeFromName ("", t) object entoryName -- ルートオブジェクトに名前はない
 
 {-
  - 指定した名前の型に一致した構成になっているか判定する
  -
- - JsonValue  判定される値
+ - JsonPair   キーと判定される値のペア
  - [JsonPair] 型名と構成情報のリスト
  - String     構成情報の型名
  - String     条件に合っていなければエラーメッセージ
  -}
-matchTypeFromName :: JsonValue -> [JsonPair] -> String -> String
-matchTypeFromName t types typeName  = matchType types (Just t) $ fromMaybe JsonNull $ lookup typeName types
+matchTypeFromName :: JsonPair -> [JsonPair] -> String -> String
+matchTypeFromName (key, t) types typeName  = matchType types (Just t) $ (key, fromMaybe JsonNull $ lookup typeName types)
 
 {-
  - 値が構成に一致しているか判定する
  -
  - [JsonPair]       構成条件のキーと値のペア
  - Maybe JsonValue  判定される値
- - JsonValue        構成情報の値
+ - JsonPair         キーと構成情報の値のペア
  - String           条件に合っていなければエラーメッセージ
  -}
-matchType :: [JsonPair] -> Maybe JsonValue -> JsonValue -> String
-matchType types t (JsonArray c)                       = case partition isMatchMessage $ map (matchType types t) c of
+matchType :: [JsonPair] -> Maybe JsonValue -> JsonPair -> String
+matchType types t (key, JsonArray c)                      = case partition isMatchMessage $ map (\v -> matchType types t (key, v)) c of
                                                         ([], e:es)  -> e      -- 配列の中のどれともマッチしなかった
                                                         _           -> mMatch -- 配列の中のどれかにマッチした
-matchType types (Just (JsonObject t)) (JsonObject c)  = case filter (not . isMatchMessage) $ map (\(key, value) -> matchType types (lookup key t) value) c of
-                                                        []    -> mMatch
-                                                        e:es  -> e
-matchType types t (JsonString c)                      = matchTypeWithString types t $ filter (/= ' ') c
-matchType _ Nothing _                                 = "error"
-matchType _ (Just t) c                                = "Not found " ++ valueToText t ++ " match pattern."
+matchType types (Just (JsonObject t)) (_, JsonObject c) = case filter (not . isMatchMessage) $ map (\(key, value) -> matchType types (lookup key t) (key, value)) c of
+                                                          []    -> mMatch
+                                                          e:es  -> e
+matchType types t (key, (JsonString c))                 = matchTypeWithString types t $ (key, filter (/= ' ') c)
+matchType _ Nothing _                                   = "error"
+matchType _ (Just t) c                                  = "Not found " ++ valueToText t ++ " match pattern."
 
 {-
  - 値が文字列で表される構成に一致しているか判定する
  -
  - [JsonPair]       構成条件のキーと値のペア
  - Maybe JsonValue  判定される値
- - String           構成情報の文字列
+ - (String, String) キーとその構成情報の文字列のペア
  - String           条件に合っていなければエラーメッセージ
  -}
-matchTypeWithString :: [JsonPair] -> Maybe JsonValue -> String -> String
-matchTypeWithString _ (Just t) "any"                          = mMatch  -- 要素があれば良い
-matchTypeWithString _ (Just (JsonBool t)) "bool"              = mMatch
-matchTypeWithString _ (Just (JsonNumber t)) "number"          = mMatch
-matchTypeWithString _ (Just (JsonString t)) "string"          = mMatch
-matchTypeWithString _ (Just JsonNull) "null"                  = mMatch
-matchTypeWithString _ Nothing "none"                          = mMatch  -- 要素が無ければ良い
-matchTypeWithString types (Just (JsonArray t)) text@('[':xs)  = maybe mMatch (matchArrayTypeWithString types t) $ bracketContent text '[' ']'
-matchTypeWithString types (Just t) c                          = matchTypeFromName t types c
-matchTypeWithString _ _ _                                     = "error"
+matchTypeWithString :: [JsonPair] -> Maybe JsonValue -> (String, String) -> String
+matchTypeWithString _ (Just t) (_, "any")                           = mMatch  -- 要素があれば良い
+matchTypeWithString _ (Just (JsonBool t)) (_, "bool")               = mMatch
+matchTypeWithString _ (Just (JsonNumber t)) (_, "number")           = mMatch
+matchTypeWithString _ (Just (JsonString t)) (_, "string")           = mMatch
+matchTypeWithString _ (Just JsonNull) (_, "null")                   = mMatch
+matchTypeWithString _ Nothing (_, "none")                           = mMatch  -- 要素が無ければ良い
+matchTypeWithString types (Just (JsonArray t)) (key, text@('[':xs)) = maybe mMatch (matchArrayTypeWithString types (key, t)) $ bracketContent text '[' ']'
+matchTypeWithString types (Just t) (key, c)                         = matchTypeFromName (key, t) types c
+matchTypeWithString _ _ (key, _)                                    = "Not found " ++ key ++ " key."
 
 {-
  - 配列の値が構成に一致しているか判定する
  -
- - [JsonPair] 構成条件のキーと値のペア
- - JsonArray  判定される配列の値
- - String     構成情報の文字列
- - String     条件に合っていなければエラーメッセージ
+ - [JsonPair]           構成条件のキーと値のペア
+ - (String, JsonArray)  キーと判定される配列の値のペア
+ - String               構成情報の文字列
+ - String               条件に合っていなければエラーメッセージ
  -}
-matchArrayTypeWithString :: [JsonPair] -> JsonArray -> String -> String
-matchArrayTypeWithString types t c  = case partition isMatchMessage $ map (\target -> matchTypeWithString types (Just target) c) t of
-                                      (_, e:es) -> (valueToText $ (\x -> (JsonArray x)) t) ++ " -- " ++ e      -- 配列の中のどれかがマッチしなかった
-                                      _         -> mMatch -- 配列の中の全てがマッチした
+matchArrayTypeWithString :: [JsonPair] -> (String, JsonArray) -> String -> String
+matchArrayTypeWithString types (key, t) c = case partition isMatchMessage $ map (\target -> matchTypeWithString types (Just target) (key, c)) t of
+                                            (_, e:es) -> (valueToText $ (\x -> (JsonArray x)) t) ++ " -- " ++ e      -- 配列の中のどれかがマッチしなかった
+                                            _         -> mMatch -- 配列の中の全てがマッチした
 
 {-
  - 一致しているメッセージか判定する
